03867ba10184149622521c5d4627c7045d56be45
Nathan Arthur <nathan@nathanarthur.com>
2023-06-01 15:41:58 -0400

    correct render blueprint

--- create_redirects.sh
+++ create_redirects.sh
@@ -4,8 +4,13 @@
 input_file="input.txt"
 output_file="output.yaml"
 
+# Delete the output file if it already exists
+if [ -f "$output_file" ]; then
+    rm $output_file
+fi
+
 # Open the output file and write the initial "routes:" line
-echo "routes:" > $output_file
+echo "    routes:" > $output_file
 
 # Read each line of the input file
 while read line; do
@@ -20,9 +25,19 @@ while read line; do
         # Get the current substring
         substring=${line:0:$i}
 
+        # Check if the redirect rule is a duplicate
+        if grep -q "source: /$substring" $output_file; then
+            continue
+        fi
+
+        # Check if the source and destination fields are identical
+        if [ "$substring" == "$line" ]; then
+            continue
+        fi
+
         # Write the redirect rule to the output file
-        echo "  - type: redirect" >> $output_file
-        echo "    source: /$substring" >> $output_file
-        echo "    destination: /$line" >> $output_file
+        echo "      - type: redirect" >> $output_file
+        echo "        source: /$substring" >> $output_file
+        echo "        destination: /$line" >> $output_file
     done
 done < $input_file
